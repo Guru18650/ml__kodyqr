547005233ba28ccf0399108806aa87ec7744d86f
Guru18650 <guruwszystkiego@gmail.com>
2024-01-11 17:44:40 +0100

    Dodaj pliki projektów.

---
+++ ml_kodyqr/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using System.IO;
+
+namespace ml_kodyqr
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           string dane = tEntry.Text;
+           img.Source = $"https://api.qrserver.com/v1/create-qr-code/?size=500x500&data={dane}";
+
+        }
+
+       
+
+
+    }
+}
